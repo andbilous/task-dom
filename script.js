fcf39fd00217634d3d058634d7351ee4bbe62173
andbilous <andbilous@gmail.com>
2018-07-11 00:13:10 +0300

    com5

--- script.js
+++ script.js
@@ -1,44 +1,101 @@
 let posts=[];
-let filteredPosts=[];
-
+let post={}
 let filterQuery={
   searchString:"",
-  postsCount:10,
+  postsCount:null,
   tags:"",
-  dateSort:null
+  dateSort:"descending"
 
 };
 
-function filter(posts,filterQuery){
+function filter(posts,filterQuery)
+{
   let i;
-  for(i=0;i<posts.length;i++){
-    while(i<=filterQuery.postsCount){
-      filteredPosts[i]=posts[i];
-    }
-    if(posts[i].title.includes(filterQuery.searchString)){
-      filteredPosts.push(posts[i]);
-    }
+  let filteredPosts=[];
+  if(filterQuery.postsCount==10){
+      filteredPosts=posts.slice(0,10);
+      console.log(filteredPosts);
+  }
+  if(filterQuery.postsCount==20){
+      filteredPosts=posts.slice(11,20);
+
+      console.log(filteredPosts);
+  }
+  if(filterQuery.postsCount==30){
+   filteredPosts=posts.slice(21,30);
+  }
+  if(filterQuery.postsCount==40){
+    filteredPosts=posts.slice(31,40);
+  }
+  if(filterQuery.postsCount==50){
+filteredPosts=posts.slice(41,50);
   }
 
+console.log(filteredPosts.length)
   return filteredPosts;
 };
 
+document.getElementById("tag-search").addEventListener("change",function(){
+
+});
+let datesortchoice=document.getElementById("date-sort");
+
+datesortchoice.addEventListener("change",function(){
+  let mathchedPosts=[];
+  mathchedPosts=posts;
+  if(datesortchoice.value=="ascending"){
+  mathchedPosts=  mathchedPosts.sort(function (a, b) {
+  if (a.createdAt > b.createdAt) {
+    return 1;
+  }
+  if (a.createdAt < b.createdAt) {
+    return -1;
+  }
+  return 0;
+});
+  }
+  else if(datesortchoice.value=="descending"){
+mathchedPosts=  mathchedPosts.sort(function (a, b) {
+  if (a.createdAt < b.createdAt) {
+    return 1;
+  }
+  if (a.createdAt > b.createdAt) {
+    return -1;
+  }
+  return 0;
+});
+  }
+  renderPosts(mathchedPosts);
+  
+})
+
 document.getElementById("search").addEventListener("keyup",function(){
-  filterQuery.searchString=document.getElementById("search").value.toUpperCase();
+  let searchString=document.getElementById("search").value;
+  let mathchedPosts=[];
+  for(let i=0;i<posts.length;i++){
+    if(posts[i].title.indexOf(searchString)>=0){
+      
+      mathchedPosts.push(posts[i]);
+    }
+  }
+renderPosts(mathchedPosts);
 });
 
 
 function renderPosts(filteredPosts){
-  console.log(JSON.filteredPosts[0]);
-  for(let i = 0; i < filteredPosts.length; i++) {
+let postsList=document.getElementById("posts");
+    while(postsList.hasChildNodes()){
+      postsList.removeChild(postsList.lastChild);
+    }
+  for (let i =0; i < filteredPosts.length; i++) {
     var listItem = document.createElement('li');
+    listItem.innerHTML='<span>'+i+'</span><br>';
     listItem.innerHTML +=  '<span>'+filteredPosts[i].title+'</span><br>';
-    listItem.innerHTML+='<img src='+filteredPosts[i].image+'><br>';
+    listItem.innerHTML +='<img src='+filteredPosts[i].image+'><br>';
     listItem.innerHTML +='<span>'+filteredPosts[i].description + '</span><br>';
     listItem.innerHTML +='<span class="date">'+filteredPosts[i].createdAt+ '</span><br>';
     listItem.innerHTML +='<span>'+filteredPosts[i].tags+'</span><br>';
-      //  console.log();
-      myList.appendChild(listItem);
+      postsList.appendChild(listItem);
       let closeSign = document.createTextNode("\u00D7");
       let close = document.createElement("SPAN");
       close.className = "close";
@@ -47,49 +104,28 @@ function renderPosts(filteredPosts){
     }
   };
 
-
-
-
+ // let posts=[];
   function fetchPosts(){
-    let post={}
     fetch('https://api.myjson.com/bins/152f9j')
     .then(function(response) { return response.json(); })
     .then(function(json) {
-      for(let i = 0; i < json.data.length; i++) {
-        post.title=json.data[i].title;
-        post.image=json.data[i].image;
-        post.description=json.data[i].description;
-        post.dateCreated=json.data[i].createdAt;
-        post.tags=json.data[i].tags;
-        posts.push(post);
-      }
-      
+        posts=json.data;
+       renderPosts(posts);
     })
-  };
+  }
 
-  
+function searchByWord(searchWord,filteredPosts){
 
-  window.onscroll = function ()
-  {
-    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    if(scrolled>3000){
-      filterQuery.postsCount=20;
-      renderPosts(filter(posts,filterQuery));
-    }
-    if(scrolled>6000){
-      filterQuery.postsCount=30;
-      renderPosts(filter(posts,filterQuery));
-    }
-    if(scrolled>9000){
-      filterQuery.postsCount=40;
-      renderPosts(filter(posts,filterQuery));
-    }
-    if(scrolled>12000){
-      filterQuery.postsCount=50;
-      renderPosts(filter(posts,filterQuery));
-    }
-  };
-  fetchPosts();
-  console.log();
-  renderPosts(posts);
+}
+function searchByTags(tags,filteredPosts){
+
+}
+
+
+
+
+
+fetchPosts();
+
+//  setTimeout(renderPosts(fetchPosts()),1000);
 
